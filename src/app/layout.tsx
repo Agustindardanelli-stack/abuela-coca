7b11a07681a3ad0dddfeb41032194b0743a2eecf
Agustindardanelli-stack <agusdardanelli16@gmail.com>
2025-08-19 19:28:27 -0300

    add SpeedInsights  the web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -101,6 +102,7 @@ export default function RootLayout({
           }}
         />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
